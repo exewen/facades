d9d216d455cf879d224e441cdb889e765b4db39d
exewen <exeweb@163.com>
2024-07-30 09:35:00 +0800

    perf: 更新请求方法

---
+++ src/HttpFacade.php
@@ -0,0 +1,29 @@
+<?php
+declare(strict_types=1);
+
+namespace Exewen\Facades;
+
+use Exewen\Http\Contract\HttpClientInterface;
+use Exewen\Http\HttpProvider;
+
+/**
+ * @method static string get(string $driver, string $uri, array $params = [], array $header = [], array $options = [])
+ * @method static string head(string $driver, string $uri, array $params = [], array $header = [], array $options = [])
+ * @method static string post(string $driver, string $uri, array $params = [], array $header = [], array $options = [], string $type = 'json')
+ * @method static string put(string $driver, string $uri, array $params = [], array $header = [], array $options = [], string $type = 'json')
+ * @method static string patch(string $driver, string $uri, array $params = [], array $header = [], array $options = [], string $type = 'json')
+ * @method static string delete(string $driver, string $uri, array $params = [], array $header = [], array $options = [])
+ * @method static string options(string $driver, string $uri, array $params = [], array $header = [], array $options = [])
+ */
+class HttpFacade extends Facade
+{
+    public static function getFacadeAccessor(): string
+    {
+        return HttpClientInterface::class;
+    }
+
+    public static function getProviders(): array
+    {
+        return [HttpProvider::class];
+    }
+}
