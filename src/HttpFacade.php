afa04e074fea6ef571dbbfb0688bea90501693e9
exewen <exeweb@163.com>
2024-07-30 10:53:22 +0800

    perf: 更新http枚举

--- src/HttpFacade.php
+++ src/HttpFacade.php
@@ -3,15 +3,16 @@ declare(strict_types=1);
 
 namespace Exewen\Facades;
 
+use Exewen\Http\Constants\HttpEnum;
 use Exewen\Http\Contract\HttpClientInterface;
 use Exewen\Http\HttpProvider;
 
 /**
  * @method static string get(string $driver, string $uri, array $params = [], array $header = [], array $options = [])
  * @method static string head(string $driver, string $uri, array $params = [], array $header = [], array $options = [])
- * @method static string post(string $driver, string $uri, array $params = [], array $header = [], array $options = [], string $type = 'json')
- * @method static string put(string $driver, string $uri, array $params = [], array $header = [], array $options = [], string $type = 'json')
- * @method static string patch(string $driver, string $uri, array $params = [], array $header = [], array $options = [], string $type = 'json')
+ * @method static string post(string $driver, string $uri, array $params = [], array $header = [], array $options = [], string $type = HttpEnum::TYPE_JSON)
+ * @method static string put(string $driver, string $uri, array $params = [], array $header = [], array $options = [], string $type = HttpEnum::TYPE_JSON)
+ * @method static string patch(string $driver, string $uri, array $params = [], array $header = [], array $options = [], string $type = HttpEnum::TYPE_JSON)
  * @method static string delete(string $driver, string $uri, array $params = [], array $header = [], array $options = [])
  * @method static string options(string $driver, string $uri, array $params = [], array $header = [], array $options = [])
  */
